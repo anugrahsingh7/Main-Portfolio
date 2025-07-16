93545942a9c9f9d4256a28efec09444de1842cbf
anugrahsingh07 <anugrahsingh7777@gmail.com>
2025-07-16 22:25:36 +0530

    font size error solved

--- src/app/MyTeam/page.js
+++ src/app/MyTeam/page.js
@@ -116,7 +116,7 @@ function MyTeam() {
               style={{ fontSize: "clamp(0.5rem, 2vw, 3rem)" }}
               >Anugrah Singh</div>
               <p className="text-center max-w-[240px] sm:max-w-[280px] md:max-w-[320px] lg:max-w-[360px] text-white/90  leading-relaxed font-light hover:text-white transition-all duration-300 drop-shadow-[0_1px_1px_rgba(0,0,0,0.9)]"
-              style={{ fontSize: "clamp(0.375rem, 1.5vw, 2.25rem)" }}
+               style={{ fontSize: "clamp(0.3rem, 1.3vw, 2.rem)" }}
               >I'm a passionate web developer with hands-on experience in modern technologies like GSAP, Locomotive Scroll, Lenis, Spline, Framer Motion, and Three.js. I specialize in creating smooth, interactive, and visually captivating user experiences using React, Next.js, and MongoDB.</p>
               
               <div className="flex gap-2 sm:gap-3">
@@ -152,7 +152,7 @@ function MyTeam() {
               <div className={`${anton.className}  font-bold text-red-400 transform group-hover:scale-110 transition-duration-300 drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)] text-center`}
               style={{ fontSize: "clamp(0.5rem, 2vw, 3rem)" }}>Vaibhav Sinha</div>
               <p className="text-center max-w-[240px] sm:max-w-[280px] md:max-w-[320px] lg:max-w-[360px] text-white/90 leading-relaxed font-light hover:text-white transition-all duration-300 drop-shadow-[0_1px_1px_rgba(0,0,0,0.9)]"
-               style={{ fontSize: "clamp(0.375rem, 1.5vw, 2.25rem)" }}
+               style={{ fontSize: "clamp(0.3rem, 1.3vw, 2.rem)" }}
               >Vaibhav is a proficient Full Stack Developer with deep expertise in React, Next.js, Express, Node.js, and MongoDB. With a solid foundation in Computer Science, he excels at building innovative, scalable solutions and driving technical excellence across the stack.</p>
               
               <div className="flex gap-2 sm:gap-3">
@@ -188,7 +188,7 @@ function MyTeam() {
               <div className={`${anton.className}  font-bold text-red-400 transform group-hover:scale-110 transition-duration-300 drop-shadow-[0_2px_2px_rgba(0,0,0,0.8)] text-center`}
               style={{ fontSize: "clamp(0.5rem, 2vw, 3rem)" }}>Sumit Joshi</div>
               <p className="text-center max-w-[240px] sm:max-w-[280px] md:max-w-[320px] lg:max-w-[360px] text-white/90 leading-relaxed font-light hover:text-white transition-all duration-300 drop-shadow-[0_1px_1px_rgba(0,0,0,0.9)]"
-               style={{ fontSize: "clamp(0.375rem, 1.5vw, 2.25rem)" }}>
+                style={{ fontSize: "clamp(0.3rem, 1.3vw, 2.rem)" }}>
                 Sumit is an aspiring software developer with hands-on experience in the MERN stack, he is passionate about building scalable web applications. With a strong foundation in C++, data structures, and algorithms, he enjoy solving complex problems and optimising code efficiency.</p>
               
               <div className="flex gap-2 sm:gap-3">
